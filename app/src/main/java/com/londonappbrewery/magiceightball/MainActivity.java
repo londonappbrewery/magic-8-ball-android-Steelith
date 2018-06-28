ecce3582daa82ae7598740ecb20f28d22480acfc
Robert <kisielarobert@gmail.com>
2018-06-28 21:03:51 +0100

    Design layout and code a .java file

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,12 +2,31 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void runMagicBall (View view) {
+        Random random = new Random();
+        int randomNumber = random.nextInt(5);
+
+        final ImageView magicBall = findViewById(R.id.ball_image_id);
+        final int[] imageArray = {
+                R.drawable.ball1,
+                R.drawable.ball2,
+                R.drawable.ball3,
+                R.drawable.ball4,
+                R.drawable.ball5,};
+
+        magicBall.setImageResource(imageArray[randomNumber]);
     }
 }
